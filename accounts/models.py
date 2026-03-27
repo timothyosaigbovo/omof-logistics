17fbdad7b28aa3d7af8d418155039f427cad75ba
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-27 00:16:05 +0000

    Add UserProfile model with company, phone, address, and country fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """Stores extra information about each user"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=254, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address_line1 = models.CharField(max_length=254, blank=True)
+    address_line2 = models.CharField(max_length=254, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    country = CountryField(blank_label='Select Country', blank=True)
+
+    def __str__(self):
+        return self.user.username
