643fc2232aabf715d533742cc9331420d8bbdea1
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-27 23:01:49 +0000

    feat(services): add service list and detail URLs

--- services/urls.py
+++ services/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'services'
+
 urlpatterns = [
     path('', views.service_list, name='service_list'),
     path('<slug:slug>/', views.service_detail, name='service_detail'),
