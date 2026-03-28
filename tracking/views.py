cc08cf1c034fd953a84a0d3b53420d780661b19f
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 00:14:14 +0000

    Add tracking_detail view showing shipment timeline

--- tracking/views.py
+++ tracking/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Shipment
 
-# Create your views here.
+
+@login_required
+def tracking_detail(request, tracking_reference):
+    shipment = get_object_or_404(
+        Shipment,
+        tracking_reference=tracking_reference,
+        order__user=request.user
+    )
+    updates = shipment.updates.all()
+    return render(request, 'tracking/tracking_detail.html', {
+        'shipment': shipment,
+        'updates': updates,
+    })
