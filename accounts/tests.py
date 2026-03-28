ddd16ce572c51e352b4d4ac06424a0eb6a5dcd60
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 06:36:09 +0000

    Add automated tests for UserProfile model and dashboard view

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Create your tests here.
+
+class UserProfileTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass123'
+        )
+
+    def test_profile_created_on_user_creation(self):
+        profile = UserProfile.objects.get(user=self.user)
+        self.assertIsNotNone(profile)
+
+    def test_profile_default_currency(self):
+        profile = UserProfile.objects.get(user=self.user)
+        self.assertEqual(profile.preferred_currency, 'GBP')
+
+    def test_dashboard_requires_login(self):
+        client = Client()
+        response = client.get('/my-account/dashboard/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_dashboard_loads_for_logged_in_user(self):
+        client = Client()
+        client.login(username='testuser', password='testpass123')
+        response = client.get('/my-account/dashboard/')
+        self.assertEqual(response.status_code, 200)
