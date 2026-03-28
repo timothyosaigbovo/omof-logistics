06fdb24a8aac2f6cdabd6ca6905af1f025b02f15
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 06:40:50 +0000

    Add automated tests for blog list, detail views and published flag

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import BlogPost
 
-# Create your tests here.
+
+class BlogTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass123'
+        )
+        self.post = BlogPost.objects.create(
+            title='Test Post',
+            slug='test-post',
+            author=self.user,
+            body='This is a test blog post.',
+            is_published=True
+        )
+
+    def test_blog_list_view(self):
+        client = Client()
+        response = client.get(reverse('blog:post_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_detail_view(self):
+        client = Client()
+        response = client.get(reverse('blog:post_detail', args=['test-post']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_unpublished_post_not_visible(self):
+        self.post.is_published = False
+        self.post.save()
+        client = Client()
+        response = client.get(reverse('blog:post_detail', args=['test-post']))
+        self.assertEqual(response.status_code, 404)
