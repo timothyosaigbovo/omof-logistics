300129f9b3eb147a63ce808b0d9a48b9704fe25b
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 00:14:14 +0000

    Register Shipment in admin with ShipmentUpdate inline

--- tracking/admin.py
+++ tracking/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Shipment, ShipmentUpdate
 
-# Register your models here.
+
+class ShipmentUpdateInline(admin.TabularInline):
+    model = ShipmentUpdate
+    extra = 1
+    readonly_fields = ('timestamp',)
+
+
+@admin.register(Shipment)
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = ('tracking_reference', 'order', 'current_status', 'estimated_delivery', 'created_at')
+    list_filter = ('current_status',)
+    search_fields = ('tracking_reference', 'order__order_number')
+    readonly_fields = ('created_at',)
+    inlines = [ShipmentUpdateInline]
