612c169047dc2bcff75c2986ecf8769f14f87b5f
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 06:36:09 +0000

    Add automated tests for Service model and service list/detail views

--- services/tests.py
+++ services/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import ServiceCategory, Service
 
-# Create your tests here.
+
+class ServiceModelTest(TestCase):
+
+    def setUp(self):
+        self.category = ServiceCategory.objects.create(
+            name='Freight', slug='freight', description='Freight services'
+        )
+        self.service = Service.objects.create(
+            category=self.category,
+            name='Air Freight',
+            slug='air-freight',
+            description='Fast air freight service',
+            is_active=True
+        )
+
+    def test_service_str(self):
+        self.assertEqual(str(self.service), 'Air Freight')
+
+    def test_service_category_str(self):
+        self.assertEqual(str(self.category), 'Freight')
+
+    def test_service_list_view(self):
+        client = Client()
+        response = client.get(reverse('services:service_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_service_detail_view(self):
+        client = Client()
+        response = client.get(reverse('services:service_detail', args=['air-freight']))
+        self.assertEqual(response.status_code, 200)
