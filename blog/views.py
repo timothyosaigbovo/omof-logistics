a58622f61b226f72e5a1dc04ff24a1b5c7cea825
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 00:22:25 +0000

    Add blog list and detail views and URLs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogPost
 
-# Create your views here.
+
+def post_list(request):
+    posts = BlogPost.objects.filter(is_published=True)
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(BlogPost, slug=slug, is_published=True)
+    return render(request, 'blog/post_detail.html', {'post': post})
