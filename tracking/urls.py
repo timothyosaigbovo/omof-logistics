3dae38759ce3447ee5198081743bdcb72b7e2a22
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 00:14:14 +0000

    Add tracking URL patterns

--- tracking/urls.py
+++ tracking/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-]
+app_name = 'tracking'
 
+urlpatterns = [
+    path('<str:tracking_reference>/', views.tracking_detail, name='tracking_detail'),
+]
