db0908ecc947238c31322e1d0e7dacb23fc06d3d
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 12:59:14 +0000

    Register QuoteRequest and Quote models in Django admin

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import QuoteRequest, Quote
 
-# Register your models here.
+
+@admin.register(QuoteRequest)
+class QuoteRequestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'service', 'origin', 'destination', 'urgency', 'status', 'submitted_at')
+    list_filter = ('status', 'urgency')
+    search_fields = ('user__username', 'origin', 'destination')
+    readonly_fields = ('submitted_at',)
+
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'quote_request', 'proposed_price', 'currency', 'valid_until', 'issued_at')
+    readonly_fields = ('issued_at',)
