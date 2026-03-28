0d9f15e0e5d3dc6689f423984e5fa2b6719e5bcf
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 00:22:22 +0000

    Add BlogPost model with slug, author, image and published flag

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='blog/', blank=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('blog:post_detail', args=[self.slug])
