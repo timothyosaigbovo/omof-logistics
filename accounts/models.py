a624ed46450c1060aba4fb77f73eeac2f013369c
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 00:20:01 +0000

    Add preferred_currency field to UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,16 @@ from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
-    """Stores extra information about each user"""
+
+    CURRENCY_CHOICES = [
+        ('GBP', 'British Pound'),
+        ('USD', 'US Dollar'),
+        ('EUR', 'Euro'),
+        ('NGN', 'Nigerian Naira'),
+        ('CAD', 'Canadian Dollar'),
+        ('GHS', 'Ghanaian Cedi'),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=254, blank=True)
     phone = models.CharField(max_length=20, blank=True)
@@ -12,6 +21,9 @@ class UserProfile(models.Model):
     address_line2 = models.CharField(max_length=254, blank=True)
     city = models.CharField(max_length=100, blank=True)
     country = CountryField(blank_label='Select Country', blank=True)
+    preferred_currency = models.CharField(
+        max_length=3, choices=CURRENCY_CHOICES, default='GBP'
+    )
 
     def __str__(self):
         return self.user.username
