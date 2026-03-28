9fc8b4626a71b716058e76a646360c589802b9f0
Timothy Osaigbovo <timothyosaigbovo@gmail.com>
2026-03-28 00:10:10 +0000

    Register Order model in admin with list display and filters

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'user', 'amount_paid', 'currency', 'status', 'created_at')
+    list_filter = ('status', 'currency')
+    search_fields = ('order_number', 'user__email', 'stripe_payment_intent_id')
+    readonly_fields = ('order_number', 'created_at')
